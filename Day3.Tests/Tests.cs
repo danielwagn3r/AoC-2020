3743054f94b3df2768943b8c0b8f2d60bbbdc5dc
Daniel Wagner <daniel@wagners.name>
2020-12-08 23:25:11 +0100

    Added Day3

---
+++ Day3.Tests/Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Day3.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestOne()
+        {
+
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day3
+{
+    public class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var input = await File.ReadAllLinesAsync("input.txt");
+        }
+    }
+}
